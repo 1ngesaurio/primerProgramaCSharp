0da3c9a782014d2eb2f7c5173583992e70d3840d
Erickson Tacam <erickson312@gmail.com>
2024-02-29 18:10:02 -0600

    Se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace primerPrograma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnImprimir = new Button();
+            SuspendLayout();
+            // 
+            // btnImprimir
+            // 
+            btnImprimir.Location = new Point(83, 92);
+            btnImprimir.Name = "btnImprimir";
+            btnImprimir.Size = new Size(115, 49);
+            btnImprimir.TabIndex = 0;
+            btnImprimir.Text = "Imprimir";
+            btnImprimir.UseVisualStyleBackColor = true;
+            btnImprimir.Click += btnImprimir_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(301, 268);
+            Controls.Add(btnImprimir);
+            Name = "Form1";
+            Text = "Inicio";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnImprimir;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace primerPrograma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo en c#");
+        }
+    }
+}
